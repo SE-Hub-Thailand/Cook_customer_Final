11f4dbad6a95eded0548563d2d1e686313cb3097
Mabelzzz <mobile44.mm@gmail.com>
2025-05-09 16:18:10 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         '/api': {
           target: 'https://cookkeptuser.sehub-thailand.com',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\\/api/, '')
+          rewrite: (path) => path.replace(/^\/api/, '')
         }
       }
     }
