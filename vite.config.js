95bd26dd1b13ca234f7bf14d4ce74c32fd3f83a6
Mabelzzz <mobile44.mm@gmail.com>
2025-05-14 14:56:44 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const isDev = process.env.NODE_ENV !== 'production'
-
-export default defineConfig({
-  plugins: [react()],
-  ...(isDev && {
-    server: {
-      port: 5173,
-      host: true,
-      strictPort: true,
-      origin: 'https://8990-2001-fb1-40-a08f-e54a-988c-b3a9-3cdd.ngrok-free.app',
-      hmr: {
-        protocol: 'wss',
-        host: '8990-2001-fb1-40-a08f-e54a-988c-b3a9-3cdd.ngrok-free.app',
-        clientPort: 443
-      },
-      proxy: {
-        '/api': {
-          target: 'https://cookkeptuser.sehub-thailand.com',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
-      }
-    }
-  }),
-  optimizeDeps: {
-    include: ['leaflet', 'react', 'react-dom', '@line/liff']
-  }
-})
-
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   // server: {
@@ -37,6 +7,20 @@ export default defineConfig({
 //   // },
 //     plugins: [react()],
 //   })
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173, // กำหนดพอร์ตที่ต้องการ
+    proxy: {
+      '/api': {
+        target: 'https://cookkeptuser.sehub-thailand.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
 
 // import react from '@vitejs/plugin-react'
 // import { defineConfig } from 'vite'
